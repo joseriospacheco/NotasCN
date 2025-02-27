e81b26cff76a920ddedcd895262303851715c49a
Jose David Rios Pacheco <=joseriospacheco@gmail.com>
2025-02-27 15:15:03 -0500

    Agregar archivos de proyecto.

---
+++ NotasCN.Application/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace NotasCN.Application;
+using NotasCN.Domain;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Ingrese las notas (0 - 5.0):");
+
+        Console.Write("Quiz 1: ");
+        double quiz1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Idea proyecto grupal: ");
+        double ideaProyectoGrupal = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Parcial 1: ");
+        double parcial1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Mesa de debate Avanzada: ");
+        double mesaDebateAvanzada = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ajuste y desarrollo avanzado del proyecto: ");
+        double ajusteDesarrolloProyecto = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Simulación del pitch final: ");
+        double simulacionPitchFinal = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Quiz 2: ");
+        double quiz2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Pitch grupal final: ");
+        double pitchGrupalFinal = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Autoevaluación: ");
+        double autoevaluacion = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Coevaluación: ");
+        double coevaluacion = Convert.ToDouble(Console.ReadLine());
+
+        double corte1 = Nota.CalcularPrimerCorte(quiz1, ideaProyectoGrupal, parcial1);
+        double corte2 = Nota.CalcularSegundoCorte(mesaDebateAvanzada, ajusteDesarrolloProyecto, simulacionPitchFinal);
+        double corte3 = Nota.CalcularTercerCorte(quiz2, pitchGrupalFinal, autoevaluacion, coevaluacion);
+
+        double notaFinal = Nota.CalcularNotaFinal(corte1, corte2, corte3);
+
+        Console.WriteLine($"Nota del Primer Corte: {corte1:F2}");
+        Console.WriteLine($"Nota del Segundo Corte: {corte2:F2}");
+        Console.WriteLine($"Nota del Tercer Corte: {corte3:F2}");
+        Console.WriteLine($"Nota Final del Curso: {notaFinal:F2}");
+
+    }
+}
---
+++ NotasCN.Domain/Nota.cs
@@ -0,0 +1,47 @@
+﻿namespace NotasCN.Domain;
+
+public class Nota
+{
+
+    public static double CalcularPrimerCorte(double quiz1, double ideaProyectoGrupal, double parcial1)
+    {
+        quiz1 = ValidarNota(quiz1);
+        ideaProyectoGrupal = ValidarNota(ideaProyectoGrupal);
+        parcial1 = ValidarNota(parcial1);
+        return ((quiz1 * 0.10) + (ideaProyectoGrupal * 0.05) + (parcial1 * 0.15))/0.3;
+    }
+
+    public static double CalcularSegundoCorte(double mesaDebateAvanzada, double ajusteDesarrolloProyecto, double simulacionPitchFinal)
+    {
+        mesaDebateAvanzada = ValidarNota(mesaDebateAvanzada);
+        ajusteDesarrolloProyecto = ValidarNota(ajusteDesarrolloProyecto);
+        simulacionPitchFinal = ValidarNota(simulacionPitchFinal);
+        return ((mesaDebateAvanzada * 0.05) + (ajusteDesarrolloProyecto * 0.20) + (simulacionPitchFinal * 0.05)) / 0.3;
+    }
+
+    public static double CalcularTercerCorte(double quiz2, double pitchGrupalFinal, double autoevaluacion, double coevaluacion)
+    {
+        quiz2 = ValidarNota(quiz2);
+        pitchGrupalFinal = ValidarNota(pitchGrupalFinal);
+        autoevaluacion = ValidarNota(autoevaluacion);
+        coevaluacion = ValidarNota(coevaluacion);
+        return ((quiz2 * 0.10) + (pitchGrupalFinal * 0.20) + (autoevaluacion * 0.05) + (coevaluacion * 0.05)) / 0.4;
+    }
+
+    public static double CalcularNotaFinal(double corte1, double corte2, double corte3)
+    {
+        return (corte1 * 0.30) + (corte2 * 0.30) + (corte3 * 0.40);
+    }
+
+    private static double ValidarNota(double nota)
+    {
+        if (nota < 0 || nota > 5)
+        {
+            throw new ArgumentOutOfRangeException("nota", "La nota debe estar entre 0 y 5.");
+        }
+        return nota;
+    }
+
+
+
+}
